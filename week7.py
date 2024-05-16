f59316eaaf3e4c1b7dbd0f69aa42574ed02891eb
harleehair <129905174+harleehair@users.noreply.github.com>
2024-05-15 19:59:21 -0700

    Create week7.py

---
+++ week7.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+rows = 10;
+cols = 10;
+
+def mandelbrot(c, z):
+   global iterations = 0;
+   count = 0
+   for a in range(iterations):
+      z = z**2 + c
+      count += 1
+      if(abs(z) > 4):
+         break
+   return count
+
+def mandelbrot_set(x, y):
+   m = np.zeros((len(x), len(y)))
+   for i in range(len(x)):
+      for j in range(len(y)):
+         c = complex(x[i], y[j])
+         z = complex(0, 0)
+         count = mandelbrot(c, z)
+         m[i, j] = count
+   return m
+
+creating our x and y arrays
+x = np.linspace(-2, 1, rows)
+y = np.linspace(-1, 1, cols)
+create our mandelbrot set
+m = mandelbrot_set(x, y) 
+plot the set (best colors: binary, hot, bone, magma)
+plt.imshow(m.T, cmap = "magma")
+plt.axis("off")
+plt.show()
